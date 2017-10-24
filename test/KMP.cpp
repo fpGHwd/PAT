139719df77619ba01f44467862cb5d87a6218c0a
Stefrex <fpGHwd@users.noreply.github.com>
2017-10-24 16:42:28 +0800

    kmp ok

--- test/KMP.cpp
+++ test/KMP.cpp
@@ -21,9 +21,9 @@ int get_index(char *s, char *t, int pos)
 }
 
 // KMP
-static char *search1 = "GOODGOOGLE";
+static char *search1 = "GOODGOOGLE"; // index = 5
 static char *pattern1 = "GOOGLE"; // next[] = {0,1,1,1,2,1};
-static char *search2 = "abababaababacb";
+static char *search2 = "abababaababacbababaaabaabaabcac"; // index = 8,15,24;
 static char *pattern2 = "ababacb"; // next[] = {0,1,1,2,3,4,1};
 static char *pattern3 = "ababaaaba"; //next[] = {0,1,1,2,3,4,2,2,3};
 static char *pattern4 = "abaabcac"; // next[] = {0,1,1,2,2,3,1,2}; //example in textbook
@@ -33,18 +33,47 @@ void get_next(char *pattern, int *next) {
 	next[i-1] = 0; // i -> index -> next[i];
 	while (i < strlen(pattern)) // i from 1 to n-1, total n-1 
 		if (j == 0 || pattern[i-1] == pattern[j-1])
-			next[i++] = ++j;
+			next[i++] = ++j; // i++;j++;next[i-1] = j;
 		else 
-			j = next[j-1]; // i++;j++;next[i-1] = j;
+			j = next[j-1]; 
+}
+
+static int next_tmp[20] = {0};
+int get_index(char *search, char *pattern) {
+	int i = 1, j = 1;// i is search index, j is pattern index
+	get_next(pattern, next_tmp);
+
+	while (i <= strlen(search) && j <= strlen(pattern)) {
+		if (j == 0 || search[i - 1] == pattern[j - 1]) {
+			i++; j++;
+		}
+		else
+			j = next_tmp[j - 1];
+	}
+
+	if (j > strlen(pattern))
+		return (i - strlen(pattern));
+	else
+		return 0;
 }
 
 void test_string(void) {
 	//int i = get_index(S, T, 0); // ok
+
+	/*
 	int next[4][20] = {0};
 	get_next(pattern1, *next);
 	get_next(pattern2, *next + 20);
 	get_next(pattern3, *next + 40);
 	get_next(pattern4, *next + 60);
+	*/
+
+	// get index
+	int i[4] = { 0 } , k = 0;
+	i[k++] = get_index(search1, pattern1);
+	i[k++] = get_index(search2, pattern2);
+	i[k++] = get_index(search2, pattern3);
+	i[k++] = get_index(search2, pattern4);
 
 	getchar();
 }
