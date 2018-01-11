b4857796f59a84e32288d1c180953da740a9f5b1
Stefrex <fpGHwd@users.noreply.github.com>
2018-01-11 18:31:32 +0800

    pat a1065

---
+++ DataStructure/PATA1065.cpp
@@ -0,0 +1,33 @@
+#include "stdafx.h" // #include <cstdio>
+
+#define N 10
+bool ret[N] = { false };
+
+bool judge(long long a, long long b, long long c) {
+	long long result;
+	result = a + b;
+	if (a > 0 && b > 0 && result < 0)
+		return true;
+	if (a < 0 && b < 0 && result > 0)
+		return false;
+	if (result > c)
+		return true;
+	else
+		return false;
+}
+
+int PATA1065() {
+	int n;
+	long long a, b, c;
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++) {
+		scanf("%lld%lld%lld", &a, &b, &c); // %lld -> %ll
+		ret[i] = judge(a, b, c);
+
+	}
+	for (int i = 0; i < n; i++) {
+		printf("Case #%d: %s\n", i+1,ret[i] ? "true" : "false");
+	}
+	
+	return 0;
+}
