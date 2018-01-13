654a5cfab63b83a76c2ba10452942728becf7860
Stefrex <fpGHwd@users.noreply.github.com>
2018-01-13 18:41:10 +0800

    PAT A1009

--- DataStructure/PATA1009.cpp
+++ DataStructure/PATA1009.cpp
@@ -1,45 +1,42 @@
 #include "stdafx.h" // #include <cstdio>
 
-#define K 1000
+#define K 1000 // 0<=NK<=1000
 
 struct term {
-	float coefficient; // coefficient
-	int exponent; // exponent
+	double coe; // coefficient
+	int exp; // exponent
 	term() {
-		coefficient = 0;
-		exponent = 0;
+		coe = 0.0;
+		exp = 0;
 	}
-}a[K],answer[2*K];
+}a[10]; // 0<=K<=10
 
+double ans[2*K+1]; // 0<=NK<=1000
 int PATA1009(void) { // int main(){}
-	int d, e; // result nonzero terms
-	scanf("%d", &d);
-	for (int i = 0; i < d; i++) {
-		scanf("%d %f", &a[i].exponent, &a[i].coefficient);
+	int m, n; // result nonzero terms
+	scanf("%d", &m);
+	for (int i = 0; i < m; i++) {
+		scanf("%d %lf", &a[i].exp, &a[i].coe); // %f -> %lf
 	}
-	scanf("%d", &e);
-	float f;
-	for (int i = 0; i < e; i++) {
-		scanf("%d %f", &e, &f);
-		for (int j = 0; j < d; j++) { // d
-			answer[e + a[j].exponent].coefficient += f * a[j].coefficient;
-			answer[e + a[j].exponent].exponent = e + a[j].exponent;
+	scanf("%d", &n);
+	int exp;
+	double cof;
+	for (int i = 0; i < n; i++) {
+		scanf("%d %lf", &exp, &cof);
+		for (int j = 0; j < m; j++) { // d
+			ans[exp + a[j].exp] += cof * a[j].coe;
+			//ans[e + a[j].exp].exp = e + a[j].exp;
 		}
 	}
 	int count = 0;
-	for (int i = 0; i <= 2 * K; i++) {
-		if (answer[i].exponent != 0)count++;
+	for (int i = 0; i <= 2 * K; i++) { // i <= 2*K -> i < 2*K or i<= 2*K -1 //  i<2*K -> i<=2*K
+		if (ans[i] != 0.0)count++;
 	}
 	
-	if (count)
-		printf("%d ", count);
-	else
-		printf("0");
-	for (int i = 2*K-1; i > 0; i--) { // count -> i
-		if (answer[i].coefficient)
-			printf("%d %.1f ", answer[i].exponent, answer[i].coefficient);
+	printf("%d", count);
+	for (int i = 2*K; i >=0; i--) { // count -> i // 0 ~ 2*K
+		if (ans[i] != 0.0)
+			printf(" %d %.1f", i, ans[i]); // direction and the blank
 	}
-	if (answer[2*K - 1].coefficient)
-		printf("%d %.1f", answer[2*K- 1].exponent, answer[2*K - 1].coefficient);
 	return 0;
 }
