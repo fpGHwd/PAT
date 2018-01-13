8197346fe001660f6b36fd0994cc83e369717a2a
Stefrex <fpGHwd@users.noreply.github.com>
2018-01-13 19:12:51 +0800

    PAT B1041

---
+++ DataStructure/PATB1041.cpp
@@ -0,0 +1,26 @@
+#include "stdafx.h" // #include<cstdio>
+
+#define N 1000
+
+struct student {
+	long long id;
+	int test;
+}D[N];
+
+int PATB1041(void) {
+	int n,tr,te;
+	long long temp;
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++) {
+		scanf("%lld%d%d", &temp, &tr, &te);
+		D[tr].id = temp;
+		D[tr].test = te;
+	}
+
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &te);
+		printf("%lld %d\n", D[te].id, D[te].test); // %ld -> %lld
+	}
+	return 0;
+}
