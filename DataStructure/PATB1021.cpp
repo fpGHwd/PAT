017aaab1d29327ef35b3e8c33ffb2952f3e01c6a
Stefrex <fpGHwd@users.noreply.github.com>
2018-01-14 22:20:47 +0800

    pat b1021

---
+++ DataStructure/PATB1021.cpp
@@ -0,0 +1,22 @@
+#include "stdafx.h"
+
+#define BASE 10
+
+int PATB1021(void) {
+	int ret[BASE] = { 0 }; // initiate to 0
+	char s[1001];
+	gets_s(s); // C just gets(char *s);
+
+	int len = strlen(s);
+	for (int i = 0; i < len; i++) {
+		ret[s[i] - '0']++; // standard
+	}
+
+	for (int i = 0; i <BASE; i++) { // 9 -> BASE
+		if (ret[i]) {
+			printf("%d:%d\n", i, ret[i]);
+		}
+	}
+
+	return 0;
+}
