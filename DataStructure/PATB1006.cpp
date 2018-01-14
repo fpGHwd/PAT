172e0705941608932d920d4d9ae42eeec632280c
Stefrex <fpGHwd@users.noreply.github.com>
2018-01-14 21:27:41 +0800

    PAT B1006

---
+++ DataStructure/PATB1006.cpp
@@ -0,0 +1,31 @@
+#include "stdafx.h"
+
+int PATB1006() {
+	int n, ret[3],count = 0;
+	char C[2] = { 'S', 'B' };
+	scanf("%d", &n);
+	for (int i = 0; i < 3 && n > 0; i++) {
+		ret[i] = n % 10;
+		n = n / 10;
+		count++;
+	}
+
+	for (int i = count-1; i >=0 ; i--) {
+		int j = 1;
+		switch (i)
+		{
+		case 2:
+			while (ret[i]--)printf("B");
+			break;
+		case 1:
+			while (ret[i]--)printf("S");
+			break;
+		case 0:
+			while (ret[i]--)printf("%d", j++);
+			break;
+		default:
+			break;
+		}
+	}
+	return 0;
+}
