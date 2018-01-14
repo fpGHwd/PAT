2536f5c7771345882657d193850f837cdd197443
Stefrex <fpGHwd@users.noreply.github.com>
2018-01-14 21:03:55 +0800

    pat a1058

---
+++ DataStructure/PATA1058.cpp
@@ -0,0 +1,18 @@
+#include "stdafx.h"
+
+#define S_G 17
+#define K_S 29
+
+int PATA1058() {
+	int a, b, c, d,e,f;
+	scanf("%d.%d.%d", &a, &b, &c);
+	scanf("%d.%d.%d", &d, &e, &f);
+	c = c + f;
+	b = b + e + c / K_S;
+	c = c % K_S;
+	a = a + d + b / S_G;
+	b = b % S_G;
+	printf("%d.%d.%d", a, b, c);
+
+	return 0;
+}
