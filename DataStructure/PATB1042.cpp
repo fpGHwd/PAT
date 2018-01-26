ac534b8b41e16bcb2feb43cbbe4f9d9eb85cf0a5
Stefrex <fpGHwd@users.noreply.github.com>
2018-01-26 21:34:41 +0800

    pat b1042

---
+++ DataStructure/PATB1042.cpp
@@ -0,0 +1,36 @@
+#include "stdafx.h"
+
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+
+#define MAX_INPUT_STRING 1000+1
+#define MAX_ASCII (2<<7-1)
+
+static char input[MAX_INPUT_STRING];
+static int statistic[MAX_ASCII] = {0};
+
+int PATB1042() {
+	freopen("input.txt", "r", stdin);
+	gets_s(input);
+	int len = strlen(input);
+	char c;
+	for (int i = 0; i < len; i++) {
+		c = input[i];
+		if (c >= 'A' && c <= 'Z') {
+			c = c - 'A' + 'a';
+		}
+		if (c >= 'a' && c <= 'z') {
+			statistic[c]++;
+		}
+	}
+
+	int max = 'a';
+	for (int i = 'a'; i <= 'z' + 1; i++) {
+		if (statistic[i] > statistic[max])max = i;
+	}
+
+	printf("%c %d", max, statistic[max]);
+
+	return 0;
+}
